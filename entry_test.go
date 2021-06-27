96ea220ea75695a787a320437e69e3a2269f5f72
Diego Savardi <diego@vipera.local>
2021-06-27 14:31:39 +0200

    Keygen + entity WIP

--- entry_test.go
+++ entry_test.go
@@ -13,23 +13,34 @@ import (
 
 func TestEntryOfFile(t *testing.T) {
 	log.SetWriter(os.Stdout)
-	bytes, err := ioutil.ReadFile("testdata/image.png")
+	// bytes, err := ioutil.ReadFile("testdata/image.png")
+	bytes, err := ioutil.ReadFile("testdata/test.txt")
+	initLen := len(bytes)
 	if err != nil {
 		t.Fatalf("error reading test file: %v", err)
 	}
-	entries, err := ddb.EntryOfFile("image.png", bytes, 1000)
-	if len(entries) < 4 {
-		t.Fatalf("Incomplete entries: %d", len(entries))
+	entries, err := ddb.EntryOfFile("test.txt", bytes, 1000)
+	if err != nil {
+		t.Fatalf("error making entries of test file: %v", err)
 	}
+	// if len(entries) != 4 {
+	// 	t.Fatalf("Wronf num of entries: %d", len(entries))
+	// }
 	var data []byte
 	var entityHash string
 	for i, f := range entries {
 		if data == nil {
-			data = make([]byte, f.Size)
+			data = make([]byte, 0)
 		}
 		t.Logf("%d size: %s  %d   hash: %s\n", i, f.Mime, len(f.Data), f.Hash)
 		data = append(data, f.Data...)
 		entityHash = f.Hash
+	}
+	endLen := len(data)
+	if initLen != endLen {
+		t.Logf("initLen != endLen => %d != %d ", initLen, endLen)
+		t.Fail()
+
 	}
 	hash := make([]byte, 64)
 	sha := sha256.Sum256(data)
@@ -38,5 +49,5 @@ func TestEntryOfFile(t *testing.T) {
 		t.Logf("the read file is wrong =>\n   %s\n!= %s", hash, entityHash)
 		t.Fail()
 	}
-	ioutil.WriteFile("readimage.png", data, 664)
+	ioutil.WriteFile("readtest.txt", data, 0664)
 }
--- keygen_test.go
+++ keygen_test.go
@@ -48,3 +48,37 @@ func TestMakeWIF(t *testing.T) {
 		fmt.Printf("WIF: %s\n", wif)
 	}
 }
+
+func TestConsitencyMakeWIF(t *testing.T) {
+	nums := []int{3567, 0, 12, 100, 1001}
+	phrases := []string{
+		"tanto va la gatta al lardo che ci lascia lo zampino",
+		"abc",
+		"cippirimerlo",
+		"un due tre stella",
+		"giro giro tondo gira tutto il mondo",
+	}
+	wifs := []string{
+		"Kxyhq2q28TjHDtQsHw5iqG9tKkzD61T9kbRA6r8PGgtgw4DN1HhB",
+		"KwDiBf89QgGbjEhKnhXJuH7LrciVrZi3qYjgd9M7rFU73Nd2Mcv1",
+		"KyhADgji8DRRGL9Sozq2Tsr3mMiVqxNuKovM1seoi3jMQJenVmTE",
+		"L5P2mJKM9MNtucYkGUT9LALtVzFPcBmmkiYeSkyPjorQ2QSFbBnj",
+		"KzWJfnzfNxd26egrsRcALpNpvBBAXnzqkK5eyAUkGcAaPCaWc4nJ",
+	}
+	for i, n := range nums {
+		k, err := ddb.NewKeygen(n, phrases[i])
+		if err != nil {
+			t.Logf("cannot generate Keygen: %v", err)
+			t.Fail()
+		}
+		wif, err := k.MakeWIF()
+		if err != nil {
+			t.Logf("cannot generate WIF: %v", err)
+			t.Fail()
+		}
+		if wif != wifs[i] {
+			t.Logf("Unexpected WIF: %s != %s\n", wif, wifs[i])
+			t.Fail()
+		}
+	}
+}
