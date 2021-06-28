f7cb57822af87c5aa7b2668244000aa3e3a44ccb
Diego <ejfhp5@gmail.com>
2021-06-28 21:01:50 +0200

    Encryption WIP

--- entry.go
+++ entry.go
@@ -1,9 +1,14 @@
 package ddb
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io"
 	"mime"
 	"path/filepath"
 
@@ -11,17 +16,23 @@ import (
 	"github.com/ejfhp/trail/trace"
 )
 
+const (
+	noncesize = 12
+)
+
+//EntryPart is the payload of a single transaction, it can contains an entire file or be a single part of a multi entry file.
 type EntryPart struct {
-	Name    string `json:"n"`
-	Hash    string `json:"h"`
-	Mime    string `json:"m"`
-	IdxPart int    `json:"i"`
-	NumPart int    `json:"t"`
-	Size    int    `json:"s"`
-	Data    []byte `json:"d"`
+	Name    string `json:"n"` //name of file
+	Hash    string `json:"h"` //hash of file
+	Mime    string `json:"m"` //mime type of file
+	IdxPart int    `json:"i"` //index of part idx of numpart
+	NumPart int    `json:"t"` //total number of parts that compose the entire file
+	Size    int    `json:"s"` //size of data
+	Data    []byte `json:"d"` //data part of the file
 }
 
-func EntryOfFile(filename string, data []byte, maxPartSize int) ([]*EntryPart, error) {
+//EntriesOfFile returns an array of entries for the given file.
+func EntriesOfFile(filename string, data []byte, maxPartSize int) ([]*EntryPart, error) {
 	t := trace.New().Source("entry.go", "Entry", "EntryOfFile")
 	log.Println(trace.Debug("building entries").UTC().Add("maxPartSize", fmt.Sprintf("%d", maxPartSize)).Append(t))
 	fm := mime.TypeByExtension(filepath.Ext(filename))
@@ -41,4 +52,67 @@ func EntryOfFile(filename string, data []byte, maxPartSize int) ([]*EntryPart, e
 		entries = append(entries, &e)
 	}
 	return entries, nil
+}
+
+//Encode return the json encoded form of rhe EntryPart
+func (e *EntryPart) Encode() ([]byte, error) {
+	data, err := json.Marshal(e)
+	if err != nil {
+		return nil, fmt.Errorf("cannot encode to json: %w", err)
+	}
+	return data, nil
+}
+
+//Decode return the EntryPart decoded from the given json
+func Decode(encoded []byte) (*EntryPart, error) {
+	var entry EntryPart
+	err := json.Unmarshal(encoded, &entry)
+	return &entry, err
+}
+
+// Key should be 32 bytes (AES-256).
+func AESEncrypt(key [32]byte, data []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key[:])
+	if err != nil {
+		return nil, fmt.Errorf("cannot initialize key: %w", err)
+	}
+
+	// Never use more than 2^32 random nonces with a given key because of the risk of a repeat.
+	nonce := make([]byte, noncesize)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, fmt.Errorf("cannot generate nonce: %w", err)
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("cannot initialize encryption: %w", err)
+	}
+
+	encoded := make([]byte, 0)
+	encoded = append(encoded, nonce...)
+	encoded = append(encoded, aesgcm.Seal(nil, nonce, data, nil)...)
+	return encoded, nil
+}
+
+// Key should be 32 bytes (AES-256).
+func AESDecrypt(key [32]byte, encrypted []byte) ([]byte, error) {
+	nonce := encrypted[:noncesize]
+	cipherdata := encrypted[noncesize:]
+
+	block, err := aes.NewCipher(key[:])
+	if err != nil {
+		return nil, fmt.Errorf("cannot initialize key: %w", err)
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("cannot initialize encryption: %w", err)
+	}
+
+	plaindata, err := aesgcm.Open(nil, nonce, cipherdata, nil)
+	if err != nil {
+		return nil, fmt.Errorf("cannot decrypt: %w", err)
+	}
+
+	return plaindata, err
 }
--- entry_test.go
+++ entry_test.go
@@ -31,7 +31,7 @@ func TestEntryOfFile(t *testing.T) {
 		if err != nil {
 			t.Fatalf("%d: error reading test file: %v", in, err)
 		}
-		entries, err := ddb.EntryOfFile(fil[0], bytes, partsizes[in][0])
+		entries, err := ddb.EntriesOfFile(fil[0], bytes, partsizes[in][0])
 		if err != nil {
 			t.Fatalf("%d: error making entries of test file: %v", in, err)
 		}
@@ -81,5 +81,49 @@ func TestEntryOfFile(t *testing.T) {
 			t.Fail()
 		}
 		ioutil.WriteFile(fmt.Sprintf("/tmp/%d.%s", in, fil[1]), data, 0664)
+	}
+}
+
+func TestEncodeDecodeEntry(t *testing.T) {
+	name := "test.txt"
+	hash := "hhhhhhhhhhhhhhhh"
+	mime := "text/plain"
+	idxPart := 1
+	numPart := 3
+	size := 30
+	data := []byte("this is the data")
+	e := ddb.EntryPart{Name: name, Hash: hash, Mime: mime, IdxPart: idxPart, NumPart: numPart, Size: size, Data: data}
+	encoded, err := e.Encode()
+	if err != nil {
+		t.Logf("Entry encoding failed: %v", err)
+		t.Fail()
+	}
+	if len(encoded) < 20 {
+		t.Logf("Encoded entry too short: %s", string(encoded))
+		t.Fail()
+	}
+	de, err := ddb.Decode(encoded)
+	if err != nil {
+		t.Logf("Entry decoding failed: %v", err)
+		t.Fail()
+	}
+	if de == nil {
+		t.Logf("Decoded entry is nil")
+		t.Fail()
+	}
+	if de.Name != name || de.Hash != hash || de.Mime != mime || de.IdxPart != idxPart || de.NumPart != numPart || de.Size != size || de.Data == nil {
+		t.Logf("Entry decoding failed, some fields doesn't match.")
+		t.Fail()
+
+	}
+}
+
+func TestEncryptDecryptText(t *testing.T) {
+	key := [32]byte(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2)
+	tests := []string{
+		{"tanto va la gatta al lardo che ci lascia lo zampino"},
+	}
+	for i, t := range tests {
+		encoded, err := ddb.AESEncrypt(key, t)
 	}
 }
