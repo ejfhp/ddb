38fb84f0b5a06d8d8436bb816b0dfcdf610f235c
ejfhp <ejfhp5@gmail.com>
2021-07-26 07:43:55 +0200

    [TRH] Passhprase test complete

--- cmd/trh/main_test.go
+++ cmd/trh/main_test.go
@@ -14,13 +14,17 @@ func TestPassphrase(t *testing.T) {
 		strings.Split("+ Bitcoin: A Peer-to-Peer Electronic Cash System - 2008 PDF", " "),
 		strings.Split("+ This is the passphrase used in the TRH help, the 24th of July, 2021.", " "),
 	}
-	nums := []int{2008, 2021}
-	keys := []string{
-		"",
-		"",
+	nums := []int{
+		2008,
+		2021,
 	}
-	pwd := [][32]byte{
+	keys := []string{
+		"L1XZMDYzVwkPUMnNgsKD8ysUMFzvvPAA1SKbeo5cjMuWSSPATQ6v",
+		"L1XkuhuE2vGfs2XSnVwr9nb4nzLcV9B2HehxUN2GiHsyAMNCpZRG",
+	}
+	pwds := [][32]byte{
 		{'B', 'i', 't', 'c', 'o', 'i', 'n', ':', ' ', 'A', ' ', 'P', 'e', 'e', 'r', '-', 't', 'o', '-', 'P', 'e', 'e', 'r', ' ', 'E', 'l', 'e', 'c', 't', 'r', 'o', 'n'},
+		{'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 't', 'h', 'e', ' ', 'p', 'a', 's', 's', 'p', 'h', 'r', 'a', 's', 'e', ' ', 'u', 's', 'e', 'd', ' ', 'i', 'n', ' ', 't'},
 	}
 	for i, pp := range passphrases {
 		pass, num, err := checkPassphrase(pp)
@@ -33,9 +37,20 @@ func TestPassphrase(t *testing.T) {
 			t.Fail()
 		}
 		key, pwd, err := keyGen(pass, num)
-		if key != keys[i][0] {
+		if err != nil {
+			t.Logf("%d keygen failed '%s' '%d': %v", i, pass, num, err)
+			t.Fail()
+		}
+		if key != keys[i] {
 			t.Logf("%d key dows't match '%s': %s", i, key, keys[i])
 			t.Fail()
+		}
+		for j, c := range pwd {
+			if c != pwds[i][j] {
+				t.Logf("%d,%d passwords doesn't match '%s': %s", i, j, string(pwd[:]), string(pwds[i][:]))
+				t.Fail()
+
+			}
 		}
 
 	}
