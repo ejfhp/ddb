2c17a5cd0e83f18274221be6d0369281057901a3
ejfhp <ejfhp5@gmail.com>
2021-07-29 21:24:18 +0200

    [trh] build and copy of zip ti ejfhp website

--- cmd/trh/build.sh
+++ cmd/trh/build.sh
@@ -8,25 +8,29 @@ export GOOS=windows
 export GOARCH=amd64 
 echo Building $GOOS $GOARCH
 go build -o trh-$GOOS-$GOARCH-$TAG.exe .
+zip trh-$GOOS-$GOARCH-$TAG.zip trh-$GOOS-$GOARCH-$TAG.exe LICENSE.md
 
 
 export GOOS=linux 
 export GOARCH=amd64 
 echo Building $GOOS $GOARCH
 go build -o  trh-$GOOS-$GOARCH-$TAG .
+zip trh-$GOOS-$GOARCH-$TAG.zip trh-$GOOS-$GOARCH-$TAG LICENSE.md
 
 
 export GOOS=darwin 
 export GOARCH=arm64 
 echo Building $GOOS $GOARCH
 go build -o trh-$GOOS-$GOARCH-$TAG .
+zip trh-$GOOS-$GOARCH-$TAG.zip trh-$GOOS-$GOARCH-$TAG LICENSE.md
 
 
 export GOOS=darwin 
 export GOARCH=amd64 
 echo Building $GOOS $GOARCH
 go build -o trh-$GOOS-$GOARCH-$TAG .
+zip trh-$GOOS-$GOARCH-$TAG.zip trh-$GOOS-$GOARCH-$TAG LICENSE.md
 
 
-mkdir  mkdir /home/diego/Code/ejfhp/ejfhp/website/static/binaries/trh/$TAG
-mv trh-* /home/diego/Code/ejfhp/ejfhp/website/static/binaries/trh/$TAG
+mkdir -p ../../../ejfhp/website/static/binaries/trh/$TAG
+mv trh-* ../../../ejfhp/website/static/binaries/trh/$TAG
